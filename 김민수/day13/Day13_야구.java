53bd84f0b41a40c9b6d81605b41677fecc126789
minsukim9900 <mmsskksw9900@gmail.com>
2025-08-08 12:09:19 +0900

    [fix] 17281. 야구

--- 김민수/day13/Day13_야구.java
+++ 김민수/day13/Day13_야구.java
@@ -26,6 +26,7 @@ public class Day13_야구 {
 		System.out.println(max);
 	}
 
+    // 야구 선수 타순 정하기
 	private static void dfs(int depth, int[] order, boolean[] visited) {
 		if (depth == 9) {
 			max = Math.max(max, game(order));
@@ -56,7 +57,9 @@ public class Day13_야구 {
 			int outCount = 0;
 			int state = 0;
 
+            // 아웃카운트 3개가 되면 이닝 종료
 			while (outCount < 3) {
+                // 타자 안타 정보
 				int runner = infos[g][order[idx++]];
 				idx %= 9;
 
@@ -65,9 +68,13 @@ public class Day13_야구 {
 					continue;
 				}
 
+                // 베이스 이동
 				state <<= (runner);
+                // 타자 베이스 이동
 				state += (1 << (runner - 1));
+                // 홈에 들어온 사람 갯수 카운트
 				score += Integer.bitCount(state >> 3);
+                // 베이스에 있는 선수만 남기기
 				state = state & 7;
 			}
 		}
