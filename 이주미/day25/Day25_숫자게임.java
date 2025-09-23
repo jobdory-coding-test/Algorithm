c9e896f289425610b0295572c806928a8d71bb58
이주미 <jmlee_1230@ewha.ac.kr>
2025-09-23 14:49:58 +0900

    [feat] [프로그래머스] 12987. 숫자 게임

---
+++ 이주미/day25/Day25_숫자게임.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Day25_숫자게임 {
+    
+    public int solution(int[] A, int[] B) {
+        int N = A.length;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int left = 0, right = N-1, numA, numB, idx;
+        boolean smallerFound;
+        for(int i=0; i<N; i++){
+            numB = B[i];
+            numA = A[left];
+            
+            if(numA < numB){
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return left;
+    }
+}
